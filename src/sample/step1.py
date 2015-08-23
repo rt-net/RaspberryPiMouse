ab227f5666d7612d4ca0734c4834b83826d159e3
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-08-23 22:39:20 +0000

    Rewrite by Ueda

--- src/sample/step1.py
+++ src/sample/step1.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python
 import time
-led0 = open('/dev/rtled0','a')
-led1 = open('/dev/rtled1','a')
-led2 = open('/dev/rtled2','a')
-led3 = open('/dev/rtled3','a')
+import sys
+
+files = ["/dev/rtled0","/dev/rtled1","/dev/rtled2","/dev/rtled3"]
 
 while 1:
-	led3.write("1")
- 	led2.write("1")
- 	led1.write("1")
- 	led0.write("1")
-	led3.flush()
-        led2.flush()
-        led1.flush()
-        led0.flush()
+	for filename in files:
+		with open(filename,'w') as f:
+			f.write("1")
+
         time.sleep(0.5)
-	led3.write("0")
-        led2.write("0")
-        led1.write("0")
-        led0.write("0")
-	led3.flush()
-       	led2.flush()
-       	led1.flush()
-      	led0.flush()
+
+	for filename in files:
+		with open(filename,'w') as f:
+			f.write("0")
+
         time.sleep(0.5)
