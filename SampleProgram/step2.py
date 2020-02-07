6c4c37a5a428f605190bf53efe9c792f7b36ee43
ShotaAk <s.aoki@rt-net.jp>
2020-02-07 07:59:07 +0000

    Refactoring to pass the pycodestyle check

--- SampleProgram/step2.py
+++ SampleProgram/step2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-import sys, tty, termios
+import sys
+import tty
+import termios
+
 
 class _Getch:
     def __init__(self):
@@ -12,17 +15,20 @@ class _Getch:
             tty.setraw(sys.stdin.fileno())
             ch = sys.stdin.read(1)
         finally:
-            termios.tcsetattr(fd,termios.TCSADRAIN,old_settings)
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
-dict= {"0":"0","a":"261","w":"277","s":"293","e":"311","d":"329","f":"349","t":"370","g":"392","y":"415","h":"440","u":"446","j":"493","k":"523"}
+
+freq_dict = {
+        "0": "0", "a": "261", "w": "277", "s": "293", "e": "311", "d": "329",
+        "f": "349", "t": "370", "g": "392", "y": "415", "h": "440", "u": "446",
+        "j": "493", "k": "523"}
 
 while 1:
     getch = _Getch()
-    d = getch()
-    if d == "c":
+    key = getch()
+    if key == "c":
         break
-    if d in dict:
-        with open('/dev/rtbuzzer0','w') as f:
-            f.write(dict[d])
-
+    if key in freq_dict:
+        with open('/dev/rtbuzzer0', 'w') as f:
+            f.write(freq_dict[key])
