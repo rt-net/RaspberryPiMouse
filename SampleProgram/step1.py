b20bdd6fc581ec62070e8a1861a745b41345fa11
ShotaAk <s.aoki@rt-net.jp>
2020-02-07 04:28:00 +0000

    Change indents of TAB (space x8) to space x4 in sample scripts.

--- SampleProgram/step1.py
+++ SampleProgram/step1.py
@@ -5,12 +5,12 @@ import sys
 files = ["/dev/rtled0","/dev/rtled1","/dev/rtled2","/dev/rtled3"]
 
 while 1:
-	for filename in files:
-		with open(filename,'w') as f:
-			f.write("1")
-	time.sleep(0.5)
-	for filename in files:
-		with open(filename,'w') as f:
-			f.write("0")
-	time.sleep(0.5)
+    for filename in files:
+        with open(filename,'w') as f:
+            f.write("1")
+        time.sleep(0.5)
+        for filename in files:
+            with open(filename,'w') as f:
+                f.write("0")
+        time.sleep(0.5)
 
--- SampleProgram/step2.py
+++ SampleProgram/step2.py
@@ -2,28 +2,28 @@
 import sys
 
 class _Getch:
-	def __init__(self):
-		import tty,sys
+    def __init__(self):
+        import tty,sys
 
-	def __call__(self):
-		import sys,tty,termios
-		fd = sys.stdin.fileno()
-		old_settings = termios.tcgetattr(fd)
-		try:
-			tty.setraw(sys.stdin.fileno())
-			ch = sys.stdin.read(1)
-		finally:
-			termios.tcsetattr(fd,termios.TCSADRAIN,old_settings)
-		return ch
+    def __call__(self):
+        import sys,tty,termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd,termios.TCSADRAIN,old_settings)
+        return ch
 
 dict= {"0":"0","a":"261","w":"277","s":"293","e":"311","d":"329","f":"349","t":"370","g":"392","y":"415","h":"440","u":"446","j":"493","k":"523"}
 
 while 1:
-	getch = _Getch()
-	d = getch()
-	if d == "c":
-		break
-	if d in dict:
-		with open('/dev/rtbuzzer0','w') as f:
-			f.write(dict[d])
+    getch = _Getch()
+    d = getch()
+    if d == "c":
+        break
+    if d in dict:
+        with open('/dev/rtbuzzer0','w') as f:
+            f.write(dict[d])
 
