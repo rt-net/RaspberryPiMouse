a11360d8fb0c719a75b5069fdc7a592a29432398
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-04-06 15:47:43 +0900

    Fix version
    Correction of https://github.com/rt-net/RaspberryPiMouse/pull/22/files#diff-3bf0890d22e6e638ddbf764783c3cce3R6

--- src/drivers/rtmouse.c
+++ src/drivers/rtmouse.c
@@ -55,7 +55,7 @@
 
 MODULE_AUTHOR("RT Corporation");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1:1.4");
+MODULE_VERSION("1:1.5");
 MODULE_DESCRIPTION("Raspberry Pi Mouse device driver");
 
 /* --- Device Numbers --- */
