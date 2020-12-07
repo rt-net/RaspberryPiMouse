7ab4afa15b2c93bf3d35c676705aac43ca71de25
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-12-07 15:39:35 +0900

    Refactor install script and support Raspberry Pi 4

--- utils/build_install.raspi4ubuntu.bash
+++ utils/build_install.raspi4ubuntu.bash
@@ -3,14 +3,16 @@
 SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0})/../; pwd)
 
 # check kernel headers
-[ ! -e /usr/src/linux ] && { bash -e $SRC_DIR/utils/print_env.bash "No kernel header files found."; exit 1; }
+[ ! -e /usr/src/linux-headers-$(uname -r) ] && { bash -e $SRC_DIR/utils/print_env.bash "No kernel header files found."; exit 1; }
 
 # build and install the driver
 cd $SRC_DIR/src/drivers/
 rm Makefile
-ln -s Makefile.raspbian Makefile
+ln -s Makefile.ubuntu14 Makefile
 make clean
-make 
+# Update for Raspberry Pi 4
+sed -i -e "s/#define RASPBERRYPI 2/#define RASPBERRYPI 4/g" rtmouse.c
+make
 sudo insmod rtmouse.ko
 
 # initialize the driver
