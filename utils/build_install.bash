bd967bc66667740573d1ea699f9550a44e6108fc
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-12-15 10:58:45 +0900

    Fix indentation
    Co-authored-by: Shota Aoki <s.aoki@rt-net.jp>

--- utils/build_install.bash
+++ utils/build_install.bash
@@ -7,14 +7,13 @@ if [ -e /usr/src/linux ]; then
 	# Raspbian/Raspberry Pi OS
 	$SRC_DIR/utils/build_install.raspbian.bash && exit 0
 elif [ "$(ls /usr/src/linux-* 2> /dev/null)" != '' ]; then
-	   # Ubuntu
-	   if grep -q "Raspberry Pi 4" /proc/cpuinfo; then
-			   $SRC_DIR/utils/build_install.raspi4ubuntu.bash && exit 0
-	   else
-			   $SRC_DIR/utils/build_install.ubuntu14.bash && exit 0
-	   fi
+	# Ubuntu
+	if grep -q "Raspberry Pi 4" /proc/cpuinfo; then
+		$SRC_DIR/utils/build_install.raspi4ubuntu.bash && exit 0
+	else
+		$SRC_DIR/utils/build_install.ubuntu14.bash && exit 0
+	fi
 else
 	# Error
 	bash -e $SRC_DIR/utils/print_env.bash "No kernel header files found."
 fi
-
