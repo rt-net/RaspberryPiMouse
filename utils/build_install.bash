a9af4fa2b2a8e34c0f93a6ce5cf88ebd50ff39c2
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-12-15 14:08:27 +0900

    Refactor install script and support Raspberry Pi 4 (#62)
    * Refactor install script and support Raspberry Pi 4
    
    * Fix indentation
    
    Co-authored-by: Shota Aoki <s.aoki@rt-net.jp>
    
    Co-authored-by: Shota Aoki <s.aoki@rt-net.jp>

--- utils/build_install.bash
+++ utils/build_install.bash
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+set -eu
 
 SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0})/../; pwd)
 
 if [ -e /usr/src/linux ]; then
+	# Raspbian/Raspberry Pi OS
 	$SRC_DIR/utils/build_install.raspbian.bash && exit 0
 elif [ "$(ls /usr/src/linux-* 2> /dev/null)" != '' ]; then
-	$SRC_DIR/utils/build_install.ubuntu14.bash && exit 0
+	# Ubuntu
+	if grep -q "Raspberry Pi 4" /proc/cpuinfo; then
+		$SRC_DIR/utils/build_install.raspi4ubuntu.bash && exit 0
+	else
+		$SRC_DIR/utils/build_install.ubuntu14.bash && exit 0
+	fi
 else
+	# Error
 	bash -e $SRC_DIR/utils/print_env.bash "No kernel header files found."
 fi
-
--- utils/build_install.ubuntu14.bash
+++ utils/build_install.ubuntu14.bash
@@ -1,13 +1,19 @@
-#!/bin/bash -vxe
+#!/bin/bash -eu
 
-dir=$(dirname $0)/../
-[ ! -e /usr/src/linux-headers-$(uname -r) ] && { bash -e $dir/utils/print_env.bash "No kernel header files found."; exit 1; }
-cd $dir/src/drivers/
+SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0})/../; pwd)
+
+# check kernel headers
+[ ! -e /usr/src/linux-headers-$(uname -r) ] && { bash -e $SRC_DIR/utils/print_env.bash "No kernel header files found."; exit 1; }
+
+# build and install the driver
+cd $SRC_DIR/src/drivers/
 rm Makefile
 ln -s Makefile.ubuntu14 Makefile
 make clean
 make
 sudo insmod rtmouse.ko
+
+# initialize the driver
 sleep 1
 sudo chmod 666 /dev/rt*
 echo 0 > /dev/rtmotoren0
