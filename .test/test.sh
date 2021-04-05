42aa236781a6948dd2a6d8d1b3ad96105c4f2198
ShotaAk <macakasit@gmail.com>
2021-04-05 15:37:22 +0900

    Use test.sh for github workflow

--- .test/test.sh
+++ .test/test.sh
@@ -2,14 +2,20 @@
 set -eu
 
 SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); cd ../; pwd)
-
 # modify here
 # ======================
 RPI_LINUX_VER=rpi-5.10.y
 CONFIG_FILE=config-5.10.11-v7+
 RPI_LINUX_COMMIT_HASH=6af8ae3
 GCC_VER=8.3
-# ======================
+
+if [ $# == 5 ]; then
+    SRC_DIR=$1
+    RPI_LINUX_VER=$2
+    CONFIG_FILE=$3
+    RPI_LINUX_COMMIT_HASH=$4
+    GCC_VER=$5
+fi
 
 mkdir -p ${HOME}/repo/raspberrypi
 echo "==========================================="
