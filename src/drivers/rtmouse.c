34c37d84ad73320ca9beabb0c98b09f139b92186
Daisuke Sato <daisuke.sato@rt-net.jp>
2021-07-15 19:34:12 +0900

    Bump up to 3.2.0

--- src/drivers/rtmouse.c
+++ src/drivers/rtmouse.c
@@ -3,7 +3,7 @@
  * rtmouse.c
  * Raspberry Pi Mouse device driver
  *
- * Version: 3.1.1
+ * Version: 3.2.0
  *
  * Copyright (C) 2015-2021 RT Corporation <shop@rt-net.jp>
  *
@@ -55,7 +55,7 @@
 
 MODULE_AUTHOR("RT Corporation");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("3.1.1");
+MODULE_VERSION("3.2.0");
 MODULE_DESCRIPTION("Raspberry Pi Mouse device driver");
 
 /* --- Device Numbers --- */
