0274f45c5eeddf630db91e498e4a2a662f411426
Daisuke Sato <daisuke.sato@rt-net.jp>
2021-10-22 19:23:42 +0900

    Add debug output

--- utils/build_install.bash
+++ utils/build_install.bash
@@ -6,16 +6,24 @@ SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0})/../; pwd)
 if [ -e /usr/src/linux ]; then
 	# build with linux headers installed from source
 	if grep -q "Raspberry Pi 4" /proc/cpuinfo; then
-		$SRC_DIR/utils/build_install_header_from_source_raspi4.bash && exit 0
+		echo build_install_header_from_source_raspi4.bash
+		$SRC_DIR/utils/build_install_header_from_source_raspi4.bash
+		exit 0
 	else
-		$SRC_DIR/utils/build_install_header_from_source_raspi2.bash && exit 0
+		echo build_install_header_from_source_raspi2.bash
+		$SRC_DIR/utils/build_install_header_from_source_raspi2.bash
+		exit 0
 	fi
 elif [ "$(ls /usr/src/linux-* 2> /dev/null)" != '' ]; then
 	# build with linux headers installed with apt
 	if grep -q "Raspberry Pi 4" /proc/cpuinfo; then
-		$SRC_DIR/utils/build_install_header_from_apt_raspi4.bash && exit 0
+		echo build_install_header_from_apt_raspi4.bash
+		$SRC_DIR/utils/build_install_header_from_apt_raspi4.bash
+		exit 0
 	else
-		$SRC_DIR/utils/build_install_header_from_apt_raspi2.bash && exit 0
+		echo build_install_header_from_apt_raspi2.bash
+		$SRC_DIR/utils/build_install_header_from_apt_raspi2.bash
+		exit 0
 	fi
 else
 	# Error
