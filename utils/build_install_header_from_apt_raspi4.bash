53066c2c41ba766da563b10c1a7751c0a5d60d4a
Daisuke Sato <daisuke.sato@rt-net.jp>
2021-10-27 11:43:43 +0900

    Update build install script name to match the current situation (#75)
    * Update file name to match the current situation
    
    * Add symbolic link for backward compatibility
    
    * Add debug output

---
+++ utils/build_install_header_from_apt_raspi4.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0})/../; pwd)
+
+# check kernel headers
+[ ! -e /usr/src/linux-headers-$(uname -r) ] && { bash -e $SRC_DIR/utils/print_env.bash "No kernel header files found."; exit 1; }
+
+# build and install the driver
+cd $SRC_DIR/src/drivers/
+rm Makefile
+ln -s Makefile.header_from_apt Makefile
+make clean
+# Update for Raspberry Pi 4
+sed -i -e "s/#define RASPBERRYPI 2/#define RASPBERRYPI 4/g" rtmouse.c
+make
+sudo insmod rtmouse.ko
+
+# initialize the driver
+sleep 1
+sudo chmod 666 /dev/rt*
+echo 0 > /dev/rtmotoren0
