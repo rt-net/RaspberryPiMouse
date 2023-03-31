60f85a375594c54c98134f70c049d2646b604f3a
Daisuke Sato <daisuke.sato@rt-net.jp>
2023-03-31 21:21:42 +0900

    Fix merge commit 8a42389

--- utils/build_install.raspbian.bash
+++ utils/build_install.raspbian.bash
@@ -1,18 +1,1 @@
-#!/bin/bash -eu
-
-SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0})/../; pwd)
-KERNEL_SRC=/usr/src/linux-headers-$(uname -r)
-
-# check kernel headers
-[ ! -e $KERNEL_SRC ] && { bash -e $SRC_DIR/utils/print_env.bash "No kernel header files found."; exit 1; }
-
-# build and install the driver
-cd $SRC_DIR/src/drivers/
-make clean
-make 
-sudo make install
-
-# initialize the driver
-sleep 1
-sudo chmod 666 /dev/rt*
-echo 0 > /dev/rtmotoren0
+build_install_header_from_source_raspi2.bash
