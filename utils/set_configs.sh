4625ff3c4d04d665e2c58fa83829dcd45ac3e8e3
kurasawa <kurasawa@rt-net.jp>
2024-09-05 11:31:15 +0900

    カーネルのバージョンに応じてdtoverlayの設定を挿入するかの分岐条件を追加

--- utils/set_configs.sh
+++ utils/set_configs.sh
@@ -8,14 +8,21 @@ DTPARAM='dtparam=i2c_baudrate=62500'
 # config-file PATH
 CONFIG_FILE='/boot/firmware/config.txt'
 
-# add dtoverlay-setting for "/boot/firmware/config.txt"
-if ! grep -qxF "$DTOVERLAY" "$CONFIG_FILE"; then
-    echo "$DTOVERLAY" | sudo tee -a "$CONFIG_FILE" >> /dev/null
-    echo "Add  \"$DTOVERLAY\"  > $CONFIG_FILE"
-fi
+# kernel version
+KERNEL_VERSION=$(uname -r | cut -d'.' -f1,2)
 
 # add dtparam-setting for "/boot/firmware/config.txt"
 if ! grep -qxF "$DTPARAM" "$CONFIG_FILE"; then
     echo "$DTPARAM" | sudo tee -a "$CONFIG_FILE" >> /dev/null
     echo "Add  \"$DTPARAM\"  > $CONFIG_FILE"
+fi
+
+# use device-tree-overlay when the kernel is 5.16 or higher
+if (( $(echo "$KERNEL_VERSION >= 5.16" | bc -l) )); then
+
+    # add dtoverlay-setting for "/boot/firmware/config.txt"
+    if ! grep -qxF "$DTOVERLAY" "$CONFIG_FILE"; then
+        echo "$DTOVERLAY" | sudo tee -a "$CONFIG_FILE" >> /dev/null
+        echo "Add  \"$DTOVERLAY\"  > $CONFIG_FILE"
+    fi
 fi
