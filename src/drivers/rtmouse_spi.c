c1f774a20ce0367119336ab4aaf8716961fb344e
YusukeKato <yusuke.kato@rt-net.jp>
2024-11-07 11:59:29 +0900

    rtmouse_spi.cにコメントを追加

--- src/drivers/rtmouse_spi.c
+++ src/drivers/rtmouse_spi.c
@@ -1,3 +1,25 @@
+/*
+ *
+ * rtmouse_spi.c
+ * SPI Driver
+ *
+ * Copyright (C) 2024 RT Corporation <shop@rt-net.jp>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
 #include "rtmouse.h"
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
