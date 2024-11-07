a556d624fc052f5a7f056ac7f6697611ee1c6a2b
YusukeKato <yusuke.kato@rt-net.jp>
2024-11-07 16:51:40 +0900

    lintに対応して修正

--- src/drivers/rtmouse_main.c
+++ src/drivers/rtmouse_main.c
@@ -42,29 +42,27 @@ const unsigned int NUM_DEV[ID_DEV_SIZE] = {
  * --- Device Names ---
  * used in rtmouse_dev.c
  */
-const char *NAME_DEV[ID_DEV_SIZE] = {
-    [ID_DEV_LED] = "rtled",
-    [ID_DEV_SWITCH] = "rtswitch",
-    [ID_DEV_SENSOR] = "rtlightsensor",
-    [ID_DEV_BUZZER] = "rtbuzzer",
-    [ID_DEV_MOTORRAWR] = "rtmotor_raw_r",
-    [ID_DEV_MOTORRAWL] = "rtmotor_raw_l",
-    [ID_DEV_MOTOREN] = "rtmotoren",
-    [ID_DEV_MOTOR] = "rtmotor"};
+const char *NAME_DEV[ID_DEV_SIZE] = {[ID_DEV_LED] = "rtled",
+				     [ID_DEV_SWITCH] = "rtswitch",
+				     [ID_DEV_SENSOR] = "rtlightsensor",
+				     [ID_DEV_BUZZER] = "rtbuzzer",
+				     [ID_DEV_MOTORRAWR] = "rtmotor_raw_r",
+				     [ID_DEV_MOTORRAWL] = "rtmotor_raw_l",
+				     [ID_DEV_MOTOREN] = "rtmotoren",
+				     [ID_DEV_MOTOR] = "rtmotor"};
 
 /*
  * --- Device Names(+%u) ---
  * used in rtmouse_dev.c
  */
-const char *NAME_DEV_U[ID_DEV_SIZE] = {
-    [ID_DEV_LED] = "rtled%u",
-    [ID_DEV_SWITCH] = "rtswitch%u",
-    [ID_DEV_SENSOR] = "rtlightsensor%u",
-    [ID_DEV_BUZZER] = "rtbuzzer%u",
-    [ID_DEV_MOTORRAWR] = "rtmotor_raw_r%u",
-    [ID_DEV_MOTORRAWL] = "rtmotor_raw_l%u",
-    [ID_DEV_MOTOREN] = "rtmotoren%u",
-    [ID_DEV_MOTOR] = "rtmotor%u"};
+const char *NAME_DEV_U[ID_DEV_SIZE] = {[ID_DEV_LED] = "rtled%u",
+				       [ID_DEV_SWITCH] = "rtswitch%u",
+				       [ID_DEV_SENSOR] = "rtlightsensor%u",
+				       [ID_DEV_BUZZER] = "rtbuzzer%u",
+				       [ID_DEV_MOTORRAWR] = "rtmotor_raw_r%u",
+				       [ID_DEV_MOTORRAWL] = "rtmotor_raw_l%u",
+				       [ID_DEV_MOTOREN] = "rtmotoren%u",
+				       [ID_DEV_MOTOR] = "rtmotor%u"};
 
 // used in by rtmouse_dev.c and cleanup_each_dev()
 int _major_dev[ID_DEV_SIZE] = {
